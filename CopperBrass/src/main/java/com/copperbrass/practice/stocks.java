2650af81ebae654ada703c971636340ee16b9c34
jimyang2 <155549852+jimyang2@users.noreply.github.com>
2024-02-13 15:08:44 +0900

    commit

---
+++ CopperBrass/src/main/java/com/copperbrass/practice/stocks.java
@@ -0,0 +1,29 @@
+package com.copperbrass.practice;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class stocks {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private Integer num;
+	
+	private String name;
+	
+	private String category;
+	
+	private String price;
+	
+	private String imgscr;
+	
+}
