554dae1befef43bf0560a307a363aa3a8c9769af
jimyang2 <155549852+jimyang2@users.noreply.github.com>
2024-02-23 16:47:08 +0900

    commit

--- CopperBrass/src/main/resources/data.sql
+++ CopperBrass/src/main/resources/data.sql
@@ -1,6 +1,6 @@
 
 Insert into stocks(id,name,category,price,imgsrc) values ('main1_1','Blooming Notebook','Notebook','$20.00','/img/main/bestseller/main1_1.JPG');
-Insert into stocks(id,name,category,price,imgsrc) values ('main1_2','Overthinker Notebook','Notebook','$20.00','/img/main/bestseller/main1_2.JPG');
+Insert into stocks(id,name,category,price,imgsrc) values ('main1_2','Overthinker Notebook','Notebook','$20.50','/img/main/bestseller/main1_2.JPG');
 Insert into stocks(id,name,category,price,imgsrc) values ('main1_3','Friendship Notebook','Notebook','$9.00','/img/main/bestseller/main1_3.JPG');
 Insert into stocks(id,name,category,price,imgsrc) values ('main1_4','School Notebook','Notebook','$8.00','/img/main/bestseller/main1_4.JPG');
 
